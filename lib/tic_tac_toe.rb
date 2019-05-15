5ad2fc5392be757f5ab2bf545046eee855849ca9
Sean Barile <aquariussb84@gmail.com>
2019-05-15 19:49:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, value)
+    move(board, index)
   else 
     turn(board)
  end
