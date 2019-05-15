6f7413824e014cdf200568c34388343a343c2358
Sean Barile <aquariussb84@gmail.com>
2019-05-15 19:45:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,3 +48,9 @@ def position_taken?(board, index)
     return true
   end
 end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  end
+end
